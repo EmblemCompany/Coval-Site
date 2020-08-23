a4dee59777e350fd613d267b445b4187dc0a462b
shannon code <shannoncode@MacBook-Pro-2.local>
2020-08-23 16:58:24 -0400

    branding

---
+++ public/main.js
@@ -0,0 +1,23 @@
+// FAQ
+$('.faq-question').on('click', function(evt) {
+    let container = $(this).parent().parent();
+    if(container.hasClass('open')) {
+        $('#coval-faq .faq-answer-content').hide();
+        container.removeClass('open');
+        return true;
+    }
+    $('#coval-faq ul li').removeClass('open');
+    $('#coval-faq .faq-answer-content').hide();
+    container.addClass('open');
+    $(container.children('.faq-answer-content')[0]).fadeIn('0.35s');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
